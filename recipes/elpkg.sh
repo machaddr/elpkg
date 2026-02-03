06fda44170307c685f54f4948a4d476bfb2c9352
André Machado <machaddr@falanet.org>
2026-02-03 11:58:37 +0000

    Fix source URL and sha256sum, and correct tar extraction in build function

--- recipes/elpkg.sh
+++ recipes/elpkg.sh
@@ -5,19 +5,19 @@ pkgname="elpkg"
 pkgver="0.1"
 pkgrel=1
 arch=("i686")
-source=("https://github.com/machaddr/elpkg/archive/refs/tags/elpkg-0.1.tar.gz")
-sha256sums=("93c0c103ce5309e85c3e0357ccd7b90ca217e7d8174544d750a14cbeed66ac6a")
+source=("https://github.com/machaddr/elpkg/archive/refs/tags/0.1.tar.gz")
+sha256sums=("5ea442dca2b72cdf22a5fb87a776307a14fbd51c00b74c59ed53a1ac7d56d9d2")
 depends=("perl" "openssl" "tar" "xz" "zstd")
 makedepends=("make")
 description="SomaLinux package manager"
 
 build() {
     cd "$srcdir"
-    tar -xzf "$srcdir/$pkgver.tar.gz"
+    tar -xzf "$srcdir/elpkg-$pkgver.tar.gz"
 }
 
 package() {
     local srcdir_pkg
-    srcdir_pkg="$srcdir/elpkg-$pkgver"
+    srcdir_pkg="$srcdir/elpkg-elpkg-$pkgver"
     make -C "$srcdir_pkg" DESTDIR="$pkgdir" install
 }
