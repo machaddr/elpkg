d7cb74007f61479a1aa8744900497cde7fb4848a
André Machado <machaddr@falanet.org>
2026-02-03 10:44:57 +0000

    Update elpkg.sh to define source and sha256sums, and improve build and package functions

--- recipes/elpkg.sh
+++ recipes/elpkg.sh
@@ -5,19 +5,19 @@ pkgname="elpkg"
 pkgver="0.1"
 pkgrel=1
 arch=("i686")
-source=()
-sha256sums=()
+source=("https://github.com/machaddr/elpkg/archive/refs/tags/elpkg-0.1.tar.gz")
+sha256sums=("93c0c103ce5309e85c3e0357ccd7b90ca217e7d8174544d750a14cbeed66ac6a")
 depends=("perl" "openssl" "tar" "xz" "zstd")
-makedepends=()
+makedepends=("make")
 description="SomaLinux package manager"
 
 build() {
-    :
+    cd "$srcdir"
+    tar -xzf "$srcdir/elpkg-$pkgver.tar.gz"
 }
 
 package() {
-    local script_dir repo_dir
-    script_dir="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-    repo_dir="$(cd -- "$script_dir/.." && pwd)"
-    make -C "$repo_dir" DESTDIR="$pkgdir" install
+    local srcdir_pkg
+    srcdir_pkg="$srcdir/elpkg-elpkg-$pkgver"
+    make -C "$srcdir_pkg" DESTDIR="$pkgdir" install
 }
