f3d0c073c0b676e578a2db1d64728dd85ced293b
André Machado <machaddr@falanet.org>
2026-03-06 19:00:03 +0000

    Update elpkg version to 0.2 and adjust source URLs for i686 and x86_64 recipes

--- recipes/i686/elpkg.sh
+++ recipes/i686/elpkg.sh
@@ -2,22 +2,22 @@
 set -euo pipefail
 
 pkgname="elpkg"
-pkgver="0.1"
+pkgver="0.2"
 pkgrel=1
 arch=("i686")
-source=("https://github.com/machaddr/elpkg/archive/refs/tags/0.1.tar.gz")
-sha256sums=("5ea442dca2b72cdf22a5fb87a776307a14fbd51c00b74c59ed53a1ac7d56d9d2")
+source=("https://github.com/machaddr/elpkg/archive/refs/tags/v$pkgver.tar.gz")
+sha256sums=("4401d83bade3b15f09778276c5e29c8d0404340e477b21cf0400aa74001d128a")
 depends=("perl" "openssl" "tar" "xz" "zstd")
 makedepends=("make")
 description="SomaLinux package manager"
 
 build() {
     cd "$srcdir"
-    tar -xzf "$srcdir/elpkg-$pkgver.tar.gz"
+    tar -xzf "$srcdir/v$pkgver.tar.gz"
 }
 
 package() {
     local srcdir_pkg
-    srcdir_pkg="$srcdir/elpkg-elpkg-$pkgver"
+    srcdir_pkg="$srcdir/elpkg-$pkgver"
     make -C "$srcdir_pkg" DESTDIR="$pkgdir" install
 }
