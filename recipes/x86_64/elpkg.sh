356d7d318c5f21a9c841bd8b3e84b40fdfd81263
André Machado <machaddr@falanet.org>
2026-03-08 10:20:54 +0000

    Update elpkg version to 0.3.1 and fix SHA256 checksums for i686 and x86_64 recipes

--- recipes/x86_64/elpkg.sh
+++ recipes/x86_64/elpkg.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 pkgname="elpkg"
-pkgver="0.3"
+pkgver="0.3.1"
 pkgrel=1
 arch=("x86_64")
 source=("https://github.com/machaddr/elpkg/archive/refs/tags/v$pkgver.tar.gz")
-sha256sums=("1d464b91b79c70deaf26e290de3b1e72fe5846646b6aaf43f461041e60f43f97")
+sha256sums=("32ad9cba0ba26cd33288cf657b7aa87d07fbebf1deed32b7e36499bec0941bc2")
 depends=("perl" "openssl" "tar" "xz" "zstd")
 makedepends=("make")
 description="SomaLinux package manager"
