c67431c49fca4384a762a6ed50d8961a38ec423f
André Machado <machaddr@falanet.org>
2026-04-03 15:43:13 +0100

    Update vte recipe to increment pkgrel to 2 and add gcc15 compatibility patch

--- recipes/DE/vte.sh
+++ recipes/DE/vte.sh
@@ -3,18 +3,19 @@ set -euo pipefail
 
 pkgname="vte"
 pkgver="0.28.2"
-pkgrel=1
+pkgrel=2
 arch=("x86_64" "i686")
 source=("https://download.gnome.org/sources/vte/0.28/vte-$pkgver.tar.xz")
 sha256sums=("SKIP")
 depends=("glib" "gtk2" "pango")
-makedepends=("glib" "gtk2" "pango" "pkgconf")
+makedepends=("glib" "gtk2" "patch" "pango" "pkgconf")
 description="Virtual terminal emulator widget"
 
 build() {
     cd "$srcdir"
     tar -xf "$srcdir/vte-$pkgver.tar.xz"
     cd "$srcdir/vte-$pkgver"
+    patch -Np1 -i "$patchdir/vte-$pkgver-gcc15_fixes-1.patch"
 
     ./configure \
         --prefix=/usr \
