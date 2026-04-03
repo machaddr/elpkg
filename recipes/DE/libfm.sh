e8e3b5f1cb2145cf64168f1dd28e32358292961f
André Machado <machaddr@falanet.org>
2026-04-03 14:59:37 +0100

    Update libfm recipe to increment pkgrel to 3 and add libfm-extra as a dependency

--- recipes/DE/libfm.sh
+++ recipes/DE/libfm.sh
@@ -3,13 +3,12 @@ set -euo pipefail
 
 pkgname="libfm"
 pkgver="1.4.1"
-pkgrel=2
+pkgrel=3
 arch=("x86_64" "i686")
 source=("https://github.com/lxde/libfm/archive/refs/tags/$pkgver.tar.gz")
 sha256sums=("SKIP")
-depends=("glib" "gtk2" "menu-cache")
+depends=("glib" "gtk2" "libfm-extra" "menu-cache")
 makedepends=("autoconf" "automake" "gettext" "glib" "gtk2" "intltool" "libtool" "menu-cache" "pkgconf")
-provides=("libfm-extra=$pkgver")
 description="Core file management library used by PCManFM and LXPanel"
 
 build() {
@@ -47,4 +46,17 @@ package() {
     build_root="$(find "$srcdir" -maxdepth 1 -mindepth 1 -type d -name 'libfm-*' | head -n 1)"
     cd "$build_root"
     make DESTDIR="$pkgdir" install
+
+    # Keep the bootstrap libfm-extra package as the owner of the extra library,
+    # pkg-config file, and extra-only headers to avoid file ownership conflicts.
+    rm -f \
+        "$pkgdir/usr/include/libfm" \
+        "$pkgdir/usr/include/libfm-1.0/fm-extra.h" \
+        "$pkgdir/usr/include/libfm-1.0/fm-version.h" \
+        "$pkgdir/usr/include/libfm-1.0/fm-xml-file.h" \
+        "$pkgdir/usr/lib/libfm-extra.a" \
+        "$pkgdir/usr/lib/libfm-extra.la" \
+        "$pkgdir/usr/lib/libfm-extra.so" \
+        "$pkgdir/usr/lib/pkgconfig/libfm-extra.pc"
+    rm -f "$pkgdir"/usr/lib/libfm-extra.so.*
 }
