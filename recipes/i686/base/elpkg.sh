89fbb05a51e8da09514e791d8b0014239ced8919
André Machado <machaddr@falanet.org>
2026-04-03 16:51:30 +0100

    Update elpkg recipe to version 0.5.2 and update sha256sums

--- recipes/i686/base/elpkg.sh
+++ recipes/i686/base/elpkg.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 pkgname="elpkg"
-pkgver="0.5.1"
+pkgver="0.5.2"
 pkgrel=1
 arch=("i686")
 source=("https://github.com/machaddr/elpkg/archive/refs/tags/v$pkgver.tar.gz")
-sha256sums=("309cbab42a78de1a89a15869b8fe7aec83038f4ee808cf9698c508a766cda257")
+sha256sums=("d53eeec33388692bb8d5848d72dc1cc7267a08e6071d757e3e80737099e78485")
 depends=("dbd-sqlite" "dbi" "openssl" "perl" "sqlite" "tar" "xz" "zstd")
 makedepends=("make")
 description="SomaLinux package manager"
