6f66eb48608a4f6fc6242676dbcce5387b534feb
André Machado <machaddr@falanet.org>
2026-04-03 15:09:33 +0100

    Update xmms2 recipe to increment pkgrel to 2 and change source URL format

--- recipes/DE/xmms2.sh
+++ recipes/DE/xmms2.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 pkgname="xmms2"
 pkgver="0.9.7"
-pkgrel=1
+pkgrel=2
 arch=("x86_64" "i686")
-source=("https://github.com/xmms2/xmms2-devel/archive/refs/tags/$pkgver.tar.gz")
-sha256sums=("SKIP")
+source=("https://github.com/xmms2/xmms2-devel/releases/download/$pkgver/xmms2-$pkgver.tar.xz")
+sha256sums=("4f149d29a4823f720c2f8ce73a9a3c143756450cc3af60aa20d6f791e3f24864")
 depends=("alsa-lib" "glib")
 makedepends=("alsa-lib" "glib" "pkgconf" "python")
 description="XMMS2 daemon and client libraries required by LXMusic"
@@ -15,23 +15,23 @@ build() {
     local build_root
 
     cd "$srcdir"
-    tar -xf "$srcdir/$pkgver.tar.gz"
-    build_root="$(find "$srcdir" -maxdepth 1 -mindepth 1 -type d -name 'xmms2-devel-*' | head -n 1)"
+    tar -xf "$srcdir/xmms2-$pkgver.tar.xz"
+    build_root="$srcdir/xmms2-$pkgver"
     cd "$build_root"
 
-    ./waf configure \
+    python3 ./waf configure \
         --prefix=/usr \
         --sysconfdir=/etc \
         --with-default-output-plugin=alsa \
         --with-optionals= \
         --without-ldconfig
-    ./waf build -j"$(nproc)"
+    python3 ./waf build -j"$(nproc)"
 }
 
 package() {
     local build_root
 
-    build_root="$(find "$srcdir" -maxdepth 1 -mindepth 1 -type d -name 'xmms2-devel-*' | head -n 1)"
+    build_root="$srcdir/xmms2-$pkgver"
     cd "$build_root"
-    ./waf install --destdir="$pkgdir"
+    python3 ./waf install --destdir="$pkgdir"
 }
